41630a3e31e90ede22949c1dac09d6da7c67b114
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-04-22 16:56:51 +0200

    Fix modules: update only available modules

--- modules/data/sql/01_update_modules.sql
+++ modules/data/sql/01_update_modules.sql
@@ -3,34 +3,34 @@
 BEGIN;
 
 
-\echo '----------------------------------------------------------------------------'
-\echo 'Add EXPORTS module if not exists'
-INSERT INTO gn_commons.t_modules (
-    module_code,
-    module_label,
-    module_picto,
-    module_desc,
-    module_path,
-    module_target,
-    active_frontend,
-    active_backend,
-    module_doc_url
-)
-    SELECT
-        'EXPORTS',
-        'Exports',
-        'fa-cloud-download',
-        'Module Exports - Télécharger les données.',
-        'exports',
-        '_self',
-        true,
-        true,
-        'https://github.com/PnX-SI/gn_module_export'
-    WHERE NOT EXISTS (
-        SELECT 'X'
-        FROM gn_commons.t_modules AS tm
-        WHERE tm.module_code = 'EXPORTS'
-    ) ;
+-- \echo '----------------------------------------------------------------------------'
+-- \echo 'Add EXPORTS module if not exists'
+-- INSERT INTO gn_commons.t_modules (
+--     module_code,
+--     module_label,
+--     module_picto,
+--     module_desc,
+--     module_path,
+--     module_target,
+--     active_frontend,
+--     active_backend,
+--     module_doc_url
+-- )
+--     SELECT
+--         'EXPORTS',
+--         'Exports',
+--         'fa-cloud-download',
+--         'Module Exports - Télécharger les données.',
+--         'exports',
+--         '_self',
+--         true,
+--         true,
+--         'https://github.com/PnX-SI/gn_module_export'
+--     WHERE NOT EXISTS (
+--         SELECT 'X'
+--         FROM gn_commons.t_modules AS tm
+--         WHERE tm.module_code = 'EXPORTS'
+--     ) ;
 
 
 \echo '----------------------------------------------------------------------------'
@@ -45,7 +45,7 @@ WHERE module_code = 'SYNTHESE' ;
 \echo 'Set modules order'
 UPDATE gn_commons.t_modules SET module_order = 1 WHERE module_code = 'GEONATURE' ;
 UPDATE gn_commons.t_modules SET module_order = 2 WHERE module_code = 'ADMIN' ;
-UPDATE gn_commons.t_modules SET module_order = 3 WHERE module_code = 'EXPORTS' ;
+-- UPDATE gn_commons.t_modules SET module_order = 3 WHERE module_code = 'EXPORTS' ;
 UPDATE gn_commons.t_modules SET module_order = 4 WHERE module_code = 'METADATA' ;
 UPDATE gn_commons.t_modules SET module_order = 5 WHERE module_code = 'SYNTHESE' ;
 
