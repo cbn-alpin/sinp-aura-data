8d19336027743636731a89705584114211d2e92e
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-05-28 08:27:26 +0200

    Fix db-geonature: fix ref_sche schema

--- db-geonature/data/sql/06_fix_geo_ref.sql
+++ db-geonature/data/sql/06_fix_geo_ref.sql
@@ -8,7 +8,7 @@ BEGIN;
 \echo 'Update constraint "fk_cor_area_synthese_id_area"'
 ALTER TABLE gn_synthese.cor_area_synthese DROP CONSTRAINT IF EXISTS fk_cor_area_synthese_id_area ;
 
-ALTER TABLE gn_synthese.cor_area_synthese ADD CONSTRAINT fk_cor_area_synthese_id_area 
+ALTER TABLE gn_synthese.cor_area_synthese ADD CONSTRAINT fk_cor_area_synthese_id_area
     FOREIGN KEY (id_area) REFERENCES ref_geo.l_areas(id_area) ON UPDATE CASCADE ;
 
 
@@ -16,7 +16,7 @@ ALTER TABLE gn_synthese.cor_area_synthese ADD CONSTRAINT fk_cor_area_synthese_id
 \echo 'Update constraint "fk_cor_site_area_id_area"'
 ALTER TABLE gn_monitoring.cor_site_area DROP CONSTRAINT IF EXISTS fk_cor_site_area_id_area ;
 
-ALTER TABLE gn_monitoring.cor_site_area ADD CONSTRAINT fk_cor_site_area_id_area 
+ALTER TABLE gn_monitoring.cor_site_area ADD CONSTRAINT fk_cor_site_area_id_area
     FOREIGN KEY (id_area) REFERENCES ref_geo.l_areas(id_area) ;
 
 
@@ -26,7 +26,7 @@ TRUNCATE TABLE gn_sensitivity.cor_sensitivity_area ;
 
 ALTER TABLE gn_sensitivity.cor_sensitivity_area DROP CONSTRAINT IF EXISTS fk_cor_sensitivity_area_id_area_fkey ;
 
-ALTER TABLE gn_sensitivity.cor_sensitivity_area ADD CONSTRAINT fk_cor_sensitivity_area_id_area_fkey 
+ALTER TABLE gn_sensitivity.cor_sensitivity_area ADD CONSTRAINT fk_cor_sensitivity_area_id_area_fkey
     FOREIGN KEY (id_area) REFERENCES ref_geo.l_areas(id_area) ;
 
 
@@ -34,7 +34,7 @@ ALTER TABLE gn_sensitivity.cor_sensitivity_area ADD CONSTRAINT fk_cor_sensitivit
 \echo 'Update constraint "cor_sensitivity_area_type_id_area_type_fkey"'
 ALTER TABLE gn_sensitivity.cor_sensitivity_area_type DROP CONSTRAINT IF EXISTS cor_sensitivity_area_type_id_area_type_fkey ;
 
-ALTER TABLE gn_sensitivity.cor_sensitivity_area_type ADD CONSTRAINT cor_sensitivity_area_type_id_area_type_fkey 
+ALTER TABLE gn_sensitivity.cor_sensitivity_area_type ADD CONSTRAINT cor_sensitivity_area_type_id_area_type_fkey
     FOREIGN KEY (id_area_type) REFERENCES ref_geo.bib_areas_types(id_type) ;
 
 
@@ -50,6 +50,12 @@ ALTER TABLE ref_geo.l_areas ADD CONSTRAINT unique_id_type_area_code UNIQUE (id_t
 ALTER TABLE ref_geo.bib_areas_types DROP CONSTRAINT IF EXISTS unique_bib_areas_types_type_code ;
 
 ALTER TABLE ref_geo.bib_areas_types ADD CONSTRAINT unique_bib_areas_types_type_code UNIQUE (type_code) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Update centroid field for "reg_geo.l_areas"'
+UPDATE ref_geo.l_areas SET centroid = ST_Centroid(geom)
+WHERE centroid IS NULL ;
 
 
 \echo '-------------------------------------------------------------------------------'
