7704af7c3999261ffc0840acf442fcf6bcf2d3c5
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-12-02 16:23:11 +0100

    feat(extract): add observations count with postgis query

--- extracts/observations_count_by_departement.sql
+++ extracts/observations_count_by_departement.sql
@@ -1,3 +1,4 @@
+-- Count observation by departement with cor_area_synthese
 SELECT
     la.area_code,
     la.area_name,
@@ -10,4 +11,19 @@ FROM gn_synthese.synthese AS s
     JOIN ref_geo.bib_areas_types AS bat
         ON bat.id_type = la.id_type
 WHERE bat.type_code = 'DEP'
+GROUP BY la.area_code, la.area_name ;
+
+-- Count observation by departement with optimitzed geometry (tmp_subdivided_area)
+-- More accurate !
+-- 4mn15
+SELECT
+    la.area_code,
+	la.area_name,
+    COUNT(s.id_synthese) AS obs_nbre
+FROM gn_synthese.synthese AS s
+	JOIN ref_geo.tmp_subdivided_area AS tsa
+		ON st_intersects(st_transform(s.the_geom_point, 2154), tsa.geom)
+	JOIN ref_geo.l_areas AS la
+		ON tsa.id_area = la.id_area
+WHERE tsa.id_type = 26
 GROUP BY la.area_code, la.area_name ;
