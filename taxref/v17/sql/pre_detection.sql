456d14b1b5b68a8b307cf0712c17b929d4e091af
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2024-08-09 15:20:31 +0200

    fix(taxref/v17): ignore false conflicts

--- taxref/v17/sql/pre_detection.sql
+++ taxref/v17/sql/pre_detection.sql
@@ -1,4 +1,4 @@
--- Mise à jour TaxRef v15 vers v16 pour le SINP AURA
+-- Mise à jour TaxRef v16 vers v17 pour le SINP AURA
 BEGIN;
 
 -- Database change in TaxHub v1.14.0
@@ -40,16 +40,54 @@ ALTER TABLE gn_synthese.synthese DISABLE TRIGGER tri_update_calculate_sensitivit
 -- Drop constraint on "cd_ref" field of "t_medias" table because we change cd_ref not yet in "taxref" table.
 ALTER TABLE taxonomie.t_medias DROP CONSTRAINT IF EXISTS check_is_cd_ref ;
 
+-- -------------------------------------------------------------------------------------------------
+-- Utils functions
+CREATE OR REPLACE FUNCTION taxonomie.get_id_attribut_by_name(attName character varying)
+    RETURNS integer
+    LANGUAGE plpgsql
+    IMMUTABLE
+AS
+$function$
+    -- Function which return the id_attribut from Taxhub Attributs
+    DECLARE idAttribut integer;
+
+    BEGIN
+        SELECT INTO idAttribut id_attribut
+        FROM taxonomie.bib_attributs
+        WHERE nom_attribut = attName ;
+
+        RETURN idAttribut ;
+    END;
+$function$ ;
+
 
 -- -------------------------------------------------------------------------------------------------
 --  Manage TaxRef
 
--- Change cd_ref in current taxref (v16) to enable check_is_cd_ref on taxonomie.t_medias
+-- Add new cd_nom to current taxref (v16) to pass test
+INSERT INTO taxonomie.taxref (
+    cd_nom, id_statut, id_habitat, id_rang, regne, phylum, classe, ordre, famille, sous_famille,
+    tribu, cd_taxsup, cd_sup, cd_ref, lb_nom, lb_auteur, nom_complet, nom_complet_html, nom_valide,
+    nom_vern, nom_vern_eng, group1_inpn, group2_inpn, url, group3_inpn
+)
+    SELECT
+        cd_nom, fr, habitat::int, rang, regne, phylum, classe, ordre, famille, sous_famille,
+        tribu, cd_taxsup, cd_sup, cd_ref, lb_nom, lb_auteur, nom_complet, nom_complet_html, nom_valide,
+        nom_vern, nom_vern_eng, group1_inpn, group2_inpn, url, group3_inpn
+    FROM taxonomie.import_taxref
+    WHERE cd_nom IN (1038698, 1027822)
+ON CONFLICT (cd_nom) DO NOTHING ;
+
+-- Change cd_ref in current taxref (v16) to enable check_is_cd_ref on
+-- taxonomie.t_medias or taxonomie.cor_taxon_attribut
 -- See https://github.com/PnX-SI/TaxHub/issues/495
 UPDATE taxonomie.taxref SET cd_ref = 95557 WHERE cd_nom = 95557 ;
 UPDATE taxonomie.taxref SET cd_ref = 112736 WHERE cd_nom = 112736 ;
 UPDATE taxonomie.taxref SET cd_ref = 133622 WHERE cd_nom = 133622 ;
 
+-- Change cd_ref in current taxref (v16) to avoid Conflict caused by split and merge
+-- This updates are net necessary with branche fix/migrate_taxref !
+-- UPDATE taxonomie.taxref SET cd_ref = 718332 WHERE cd_nom = 87354 ;
 
 -- -------------------------------------------------------------------------------------------------
 -- Manage taxonomie.cdnom_disparu.cd_raison_suppression = 1, 2 or 3
@@ -76,9 +114,17 @@ UPDATE gn_synthese.synthese SET cd_nom = 614970 WHERE cd_nom = 114383 ;
 -- -------------------------------------------------------------------------------------------------
 -- Manage BIB_NOMS
 
+-- Insert new taxon in bib_nom for constraint "fk_t_media_bib_noms" of "t_medias" table.
+-- For new names retained (see "Manage TaxRef" section above here)
+INSERT INTO taxonomie.bib_noms (cd_nom, cd_ref) VALUES
+    (1038698, 1038698),
+    (1027822, 1027822),
+    (133622, 133622)
+ON CONFLICT DO NOTHING ;
+
 -- Change non compatible bib_noms rows (old cd_ref) with new TaxRef version
-UPDATE taxonomie.bib_noms SET cd_ref = 133622 WHERE cd_nom = 133970 AND cd_ref = 133970 ;
-UPDATE taxonomie.bib_noms SET cd_ref = 133622 WHERE cd_nom = 133969 AND cd_ref = 133969 ;
+UPDATE taxonomie.bib_noms SET cd_ref = 1038698 WHERE cd_nom = 133969 AND cd_ref = 133969 ;
+UPDATE taxonomie.bib_noms SET cd_ref = 95557 WHERE cd_nom = 133970 AND cd_ref = 133970 ;
 
 -- Update rows with replacement cd_nom in synthese (cd_raison_suppression = 1)
 UPDATE taxonomie.bib_noms SET cd_nom = 789254 WHERE cd_nom = 104596 ;
@@ -88,19 +134,19 @@ UPDATE taxonomie.bib_noms SET cd_nom = 978967 WHERE cd_nom = 109829 ;
 
 
 -- Deleting rows in "cor_nom_liste" for all kinds of conflicts (see below bib_noms):
--- - obsolete cd_name + cd_ref association : 88591, 93599, 93653, 133568, 95763, 103278
--- - cd_nom deleted (cd_raison_suppression = 1,2,3) : others
+-- - obsolete cd_name + cd_ref association
+-- - cd_nom deleted (cd_raison_suppression = 1,2,3)
 DELETE FROM taxonomie.cor_nom_liste AS l
 WHERE l.id_nom IN (
     SELECT id_nom
     FROM taxonomie.bib_noms
     WHERE cd_nom IN (
-        85841, 85893, 85914, 85922, 90788, 94956, 95552, 95583, 95746, 108148, 108159, 108225,
-        108258, 108284, 108288, 108290, 108324, 108997, 109517, 109692, 110477, 116017, 119138,
-        119151, 119166, 119168, 119177, 119229, 119268, 119362, 119388, 124829, 137953, 141567,
-        141571, 718789,
-        113709, 114383,
-        469860, 607025, 199049, 884045
+        95746, 119151, 119166, 119229, 119362, 119388, 137953,
+        85841, 85893, 85914, 85922, 90788, 94956, 95552, 108159, 108225, 108258, 108284, 108288,
+        108290, 108324, 108997, 109517, 110477, 116017, 119138, 119168, 119177, 119268, 124829, 141567,
+        469860, 607025, 199049, 884045,
+        95583, 108148, 109692, 141571, 718789,
+        113709, 114383
     )
 ) ;
 
@@ -109,59 +155,55 @@ WHERE l.id_nom IN (
 -- "taxonomie.bib_noms" must be recreated from gn_synthese.synthese if necessary.
 DELETE FROM taxonomie.bib_noms
 WHERE cd_nom IN (
-    85841, 85893, 85914, 85922, 90788, 94956, 95552, 95583, 95746, 108148, 108159, 108225,
-    108258, 108284, 108288, 108290, 108324, 108997, 109517, 109692, 110477, 116017, 119138,
-    119151, 119166, 119168, 119177, 119229, 119268, 119362, 119388, 124829, 137953, 141567,
-    141571, 718789,
-    113709, 114383,
-    469860, 607025, 199049, 884045
+    95746, 119151, 119166, 119229, 119362, 119388, 137953,
+    85841, 85893, 85914, 85922, 90788, 94956, 95552, 108159, 108225, 108258, 108284, 108288,
+    108290, 108324, 108997, 109517, 110477, 116017, 119138, 119168, 119177, 119268, 124829, 141567,
+    469860, 607025, 199049, 884045,
+    95583, 108148, 109692, 141571, 718789,
+    113709, 114383
 ) ;
 
 
 -- -------------------------------------------------------------------------------------------------
 -- Manage Attributs
 
+
+-- Keep attributs value of destination taxon merged
+-- Papaver dubium subsp. dubium [138564] merged in Papaver dubium [112303] :
+UPDATE taxonomie.cor_taxon_attribut AS cta1
+SET valeur_attribut = cta2.valeur_attribut
+FROM taxonomie.cor_taxon_attribut AS cta2
+WHERE cta1.cd_ref = 112303
+    AND cta1.id_attribut = taxonomie.get_id_attribut_by_name('sinp_description')
+    AND cta2.cd_ref = 138564
+    AND cta2.id_attribut = taxonomie.get_id_attribut_by_name('sinp_description') ;
+
+-- Plantago coronopus subsp. coronopus [138881] merged in Plantago coronopus [113842]
+UPDATE taxonomie.cor_taxon_attribut AS cta1
+SET valeur_attribut = cta2.valeur_attribut
+FROM taxonomie.cor_taxon_attribut AS cta2
+WHERE cta1.cd_ref = 113842
+    AND cta1.id_attribut = taxonomie.get_id_attribut_by_name('sinp_ecology')
+    AND cta2.cd_ref = 138881
+    AND cta2.id_attribut = taxonomie.get_id_attribut_by_name('sinp_ecology') ;
+
+-- Rosmarinus officinalis [118865] merged in Salvia rosmarinus [1027822]
+UPDATE taxonomie.cor_taxon_attribut SET cd_ref = 1027822 WHERE cd_ref = 118865 ;
+
 -- Remove useless attributs : cas3 two or more taxons merged
 DELETE FROM taxonomie.cor_taxon_attribut WHERE cd_ref IN (
-    131360, 131499, 140173, 118865, 138564, 97740, 138881, 719191, 126565,
-    80990, 132060, 133970, 133969, 95793, 109091, 138536, 112747, 112745, 141214, 94063, 94066,
-    145876, 109084, 111289, 138536, 120700, 718670, 788968, 718671, 91910
+    131360, 131499, 140173, 138564, 97740, 138881, 719191, 126565,
+    136979, 137161
 ) ;
+
 
 -- -------------------------------------------------------------------------------------------------
 -- Manage Media
 
 -- Associate deleted taxons photos to new taxons : cas3 two or more taxons merged
--- Antirrhinum majus subsp. majus [131499]
-UPDATE taxonomie.t_medias SET cd_ref = 83058, id_type = 2 WHERE cd_ref = 131499 ;
--- Papaver dubium subsp. dubium [138564]
-UPDATE taxonomie.t_medias SET cd_ref = 112303, id_type = 2 WHERE cd_ref = 138564 ;
--- Plantago coronopus subsp. coronopus [138881]
-UPDATE taxonomie.t_medias SET cd_ref = 113842, id_type = 2 WHERE cd_ref = 138881 ;
--- Taxus fastigiata [719191]
-UPDATE taxonomie.t_medias SET cd_ref = 125816, id_type = 2 WHERE cd_ref = 719191 ;
--- Ajuga reptans [80990]
-UPDATE taxonomie.t_medias SET cd_ref = 80988, id_type = 2 WHERE cd_ref = 80990 ;
--- Ballota nigra subsp. foetida [132060]
-UPDATE taxonomie.t_medias SET cd_ref = 85502, id_type = 2 WHERE cd_ref = 132060 ;
--- Dryopteris affinis subsp. cambrensis [133970], Dryopteris affinis subsp. borreri [133969]
-UPDATE taxonomie.t_medias SET cd_ref = 95557, id_type = 2 WHERE cd_ref IN (133970, 133969) ;
--- Echium vulgare [95793]
-UPDATE taxonomie.t_medias SET cd_ref = 95741, id_type = 2 WHERE cd_ref = 95793 ;
--- Persicaria mitis [112747], Persicaria maculosa [112745]
-UPDATE taxonomie.t_medias SET cd_ref = 112736, id_type = 2 WHERE cd_ref IN (112747, 112745) ;
--- Silene vulgaris subsp. vulgaris [141214]
-UPDATE taxonomie.t_medias SET cd_ref = 123683, id_type = 2 WHERE cd_ref = 141214 ;
--- Cystopteris fragilis [94066], Cystopteris fragilis var. fragilis [145876]
-UPDATE taxonomie.t_medias SET cd_ref = 133622, id_type = 2 WHERE cd_ref IN (94066, 145876) ;
--- Myosotis ramosissima [109084]
-UPDATE taxonomie.t_medias SET cd_ref = 137934, id_type = 2 WHERE cd_ref = 109084 ;
--- Origanum vulgare [111289]
-UPDATE taxonomie.t_medias SET cd_ref = 138444, id_type = 2 WHERE cd_ref = 111289 ;
--- Salvia verbenaca [120700]
-UPDATE taxonomie.t_medias SET cd_ref = 140527, id_type = 2 WHERE cd_ref = 120700 ;
--- Clinopodium nepeta subsp. sylvaticum [788968], Clinopodium nepeta [91910]
-UPDATE taxonomie.t_medias SET cd_ref = 718332, id_type = 2 WHERE cd_ref IN (788968, 91910) ;
+
+-- Rosmarinus officinalis [118865]
+UPDATE taxonomie.t_medias SET cd_ref = 1027822, id_type = 2 WHERE cd_ref = 118865 ;
 
 
 -- -------------------------------------------------------------------------------------------------
