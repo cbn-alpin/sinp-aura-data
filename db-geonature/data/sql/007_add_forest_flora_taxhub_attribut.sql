18011215905a2d5daf9bb94814bb0cf0dbb8e4d9
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2024-11-29 14:45:01 +0100

    feat(db-geonature): add priority forest flora attributs and data

---
+++ db-geonature/data/sql/007_add_forest_flora_taxhub_attribut.sql
@@ -0,0 +1,265 @@
+-- Add new forest_flora_priority TaxHub attribut with new thema forest_flora.
+-- Required rights: DB OWNER
+-- GeoNature database compatibility : v2.9.2+
+-- Transfert this script on server with Git or this way:
+-- rsync -av ./007_* geonat@db-aura-sinp:~/data/db-geonature/data/sql/ --dry-run
+-- Use this script this way:
+--      cat ./007_* | sed 's#${csvDirectory}#/path/to/db-geonature/csv/directory#g'| \
+--      psql -h localhost -U geonatadmin -d geonature2db
+
+BEGIN ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Create function get_id_theme_by_name'
+
+DROP FUNCTION IF EXISTS taxonomie.get_id_theme_by_name(varchar) ;
+
+CREATE OR REPLACE FUNCTION taxonomie.get_id_theme_by_name(themeName varchar)
+ RETURNS integer
+ LANGUAGE plpgsql
+ IMMUTABLE
+AS $function$
+    -- Function which return the id_theme from Taxhub Attributs theme
+    DECLARE idTheme integer;
+
+    BEGIN
+        SELECT INTO idTheme id_theme
+        FROM taxonomie.bib_themes
+        WHERE nom_theme = themeName ;
+
+        RETURN idTheme ;
+    END;
+$function$ ;
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Create temporary priority forest flora import table'
+DROP TABLE IF EXISTS gn_imports.piority_forest_flora ;
+
+CREATE TABLE gn_imports.piority_forest_flora AS
+    SELECT
+        NULL::SERIAL AS gid,
+        NULL::INT(4) AS cd_nom,
+        NULL::VARCHAR(250) AS taxa_name,
+        NULL::VARCHAR(50) AS group,
+        NULL::BOOLEAN AS lelve_1_alp,
+        NULL::BOOLEAN AS lelve_1_mc,
+        NULL::BOOLEAN AS lelve_2_alp,
+        NULL::BOOLEAN AS lelve_2_mc
+WITH NO DATA ;
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Copy Tracheophyta CVS file to temporary import table'
+ALTER TABLE gn_imports.piority_forest_flora ALTER COLUMN group SET DEFAULT 'Tracheophyta';
+
+\COPY gn_imports.piority_forest_flora (
+    cd_nom,
+    taxa_name,
+    level_1_alp,
+    level_1_mc,
+    level_2_alp,
+    level_2_mc
+)
+FROM '${csvDirectory}/priority_forest_flora_tracheophyta.csv'
+WITH CSV HEADER DELIMITER E'\t' NULL '\N' ;
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Copy Bryophyta CVS file to temporary import table'
+ALTER TABLE gn_imports.piority_forest_flora ALTER COLUMN group SET DEFAULT 'Bryophyta';
+
+\COPY gn_imports.piority_forest_flora (
+    cd_nom,
+    taxa_name,
+    level_1_alp,
+    level_1_mc
+)
+FROM '${csvDirectory}/priority_forest_flora_bryophyta.csv'
+WITH CSV HEADER DELIMITER E'\t' NULL '\N' ;
+
+ALTER TABLE gn_imports.piority_forest_flora ALTER COLUMN group DROP DEFAULT;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Delete all previous forest_flora topic data'
+
+DELETE FROM taxonomie.cor_taxon_attribut
+WHERE id_attribut IN (
+    SELECT id_attribut
+    FROM taxonomie.bib_attributs
+    WHERE id_theme = taxonomie.get_id_theme_by_name('forest_flora')
+) ;
+
+DELETE FROM taxonomie.bib_attributs
+WHERE id_theme = taxonomie.get_id_theme_by_name('forest_flora') ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Create TaxHub attributes topic'
+-- Change "Mon territoire" default not used theme to "forest_flora" topic
+UPDATE taxonomie.bib_themes SET
+    nom_theme = 'forest_flora',
+    desc_theme =  'Informations relatives à la flore forestière.'
+WHERE nom_theme = 'Mon territoire' ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Create TaxHub attributs'
+INSERT INTO taxonomie.bib_attributs (
+    nom_attribut,
+    label_attribut,
+    liste_valeur_attribut,
+    obligatoire,
+    desc_attribut,
+    type_attribut,
+    type_widget,
+    id_theme,
+    ordre
+) VALUES (
+    'forest_flora_priority_tracheophyta',
+    'Flore vasculaire forestière prioritaire',
+    '{"values": ["Enjeu 1 Alpes", "Enjeu 2 Alpes", "Enjeu 1 Massif Central", "Enjeu 2 Massif Central"]}',
+    false,
+    'Priorité des taxons de la flore vasculaire forestière sur la région AURA.',
+    'text',
+    'multiselect',
+    taxonomie.get_id_theme_by_name('forest_flora'),
+    1
+), (
+    'forest_flora_priority_bryophyta',
+    'Bryoflore forestière prioritaire',
+    '{"values": ["Enjeu Alpes", "Enjeu Massif Central"]}',
+    false,
+    'Priorité des taxons de la bryoflore forestière sur la région AURA.',
+    'text',
+    'multiselect',
+    taxonomie.get_id_theme_by_name('forest_flora'),
+    2
+) ON CONFLICT (nom_attribut) DO NOTHING ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Associate TaxHub attribut forest_flora_priority_tracheophyta="Enjeu 1 Alpes" to taxons'
+INSERT INTO taxonomie.cor_taxon_attribut (
+    id_attribut,
+    valeur_attribut,
+    cd_ref
+)
+    SELECT DISTINCT
+        taxonomie.get_id_attribut_by_name('forest_flora_priority_tracheophyta'),
+        'Enjeu 1 Alpes',
+        t.cd_ref
+    FROM taxonomie.taxref AS t
+    WHERE t.cd_nom IN (
+        SELECT cd_nom
+        FROM gn_imports.piority_forest_flora
+        WHERE group = 'Tracheophyta'
+            AND level_1_alp = TRUE
+    ) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Associate TaxHub attribut forest_flora_priority_tracheophyta="Enjeu 2 Alpes" to taxons'
+INSERT INTO taxonomie.cor_taxon_attribut (
+    id_attribut,
+    valeur_attribut,
+    cd_ref
+)
+    SELECT DISTINCT
+        taxonomie.get_id_attribut_by_name('forest_flora_priority_tracheophyta'),
+        'Enjeu 2 Alpes',
+        t.cd_ref
+    FROM taxonomie.taxref AS t
+    WHERE t.cd_nom IN (
+        SELECT cd_nom
+        FROM gn_imports.piority_forest_flora
+        WHERE group = 'Tracheophyta'
+            AND level_2_alp = TRUE
+    ) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Associate TaxHub attribut forest_flora_priority_tracheophyta="Enjeu 1 Massif Central" to taxons'
+INSERT INTO taxonomie.cor_taxon_attribut (
+    id_attribut,
+    valeur_attribut,
+    cd_ref
+)
+    SELECT DISTINCT
+        taxonomie.get_id_attribut_by_name('forest_flora_priority_tracheophyta'),
+        'Enjeu 1 Massif Central',
+        t.cd_ref
+    FROM taxonomie.taxref AS t
+    WHERE t.cd_nom IN (
+        SELECT cd_nom
+        FROM gn_imports.piority_forest_flora
+        WHERE group = 'Tracheophyta'
+            AND level_1_mc = TRUE
+    ) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Associate TaxHub attribut forest_flora_priority_tracheophyta="Enjeu 2 Massif Central" to taxons'
+INSERT INTO taxonomie.cor_taxon_attribut (
+    id_attribut,
+    valeur_attribut,
+    cd_ref
+)
+    SELECT DISTINCT
+        taxonomie.get_id_attribut_by_name('forest_flora_priority_tracheophyta'),
+        'Enjeu 2 Massif Central',
+        t.cd_ref
+    FROM taxonomie.taxref AS t
+    WHERE t.cd_nom IN (
+        SELECT cd_nom
+        FROM gn_imports.piority_forest_flora
+        WHERE group = 'Tracheophyta'
+            AND level_2_mc = TRUE
+    ) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Associate TaxHub attribut forest_flora_priority_bryophyta="Enjeu Alpes" to taxons'
+INSERT INTO taxonomie.cor_taxon_attribut (
+    id_attribut,
+    valeur_attribut,
+    cd_ref
+)
+    SELECT DISTINCT
+        taxonomie.get_id_attribut_by_name('forest_flora_priority_bryophyta'),
+        'Enjeu Alpes',
+        t.cd_ref
+    FROM taxonomie.taxref AS t
+    WHERE t.cd_nom IN (
+        SELECT cd_nom
+        FROM gn_imports.piority_forest_flora
+        WHERE group = 'Bryophyta'
+            AND level_1_alp = TRUE
+    ) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Associate TaxHub attribut forest_flora_priority_bryophyta="Enjeu Massif Central" to taxons'
+INSERT INTO taxonomie.cor_taxon_attribut (
+    id_attribut,
+    valeur_attribut,
+    cd_ref
+)
+    SELECT DISTINCT
+        taxonomie.get_id_attribut_by_name('forest_flora_priority_bryophyta'),
+        'Enjeu Massif Central',
+        t.cd_ref
+    FROM taxonomie.taxref AS t
+    WHERE t.cd_nom IN (
+        SELECT cd_nom
+        FROM gn_imports.piority_forest_flora
+        WHERE group = 'Bryophyta'
+            AND level_1_mc = TRUE
+    ) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'COMMIT if all is OK:'
+COMMIT ;
