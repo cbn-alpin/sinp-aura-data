405f696547d1ecc2f414bb2606d77961e30cb891
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2024-12-08 17:01:30 +0100

    fix(lpo): improve query to reinsert duplicate UUID

--- lpo/data/sql/fix/003_fix_gn2pg_error_log_duplicate_uuid.sql
+++ lpo/data/sql/fix/003_fix_gn2pg_error_log_duplicate_uuid.sql
@@ -3,6 +3,8 @@
 -- Usage: psql -h "localhost" -U "<db-owner-name>" -d "<db-name>" -f <path-to-this-sql-file>
 -- Ex.: psql -h "localhost" -U "geonatadmin" -d "geonature2db" -f ~/data/lpo/data/sql/fix/003_*
 
+\echo '----------------------------------------------------------------------------'
+\echo 'Start deleting transaction (very long)'
 BEGIN;
 
 \echo '-------------------------------------------------------------------------------'
@@ -17,13 +19,23 @@ WITH duplicated_uuid AS (
             ) ILIKE '%duplicate key value violates unique constraint "unique_id_sinp_unique"%'
 )
 DELETE FROM gn2pg_lpo.data_json
-WHERE uuid IN (SELECT id_perm_sinp FROM duplicated_uuid) ;
+WHERE "uuid" IN (SELECT id_perm_sinp FROM duplicated_uuid) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'COMMIT if all is OK:'
+COMMIT;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Start new transaction'
+BEGIN;
 
 
 \echo '-------------------------------------------------------------------------------'
 \echo 'Reinsert in data_json entries where UUID is noted as duplicate in error log'
 INSERT INTO gn2pg_lpo.data_json (
-    source,
+    "source",
     controler,
     "type",
     id_data,
@@ -31,26 +43,32 @@ INSERT INTO gn2pg_lpo.data_json (
     item
 )
 SELECT
-    source,
+    "source",
     controler,
     'synthese_with_metadata',
     id_data,
     (item ->> 'id_perm_sinp')::uuid,
     item
-FROM gn2pg_lpo.error_log
+FROM gn2pg_lpo.error_log AS el
 WHERE last_ts > '2024-11-01'
     AND split_part(
         split_part(error::text, ') '::text, 2),
         'DETAIL'::text, 1
-    ) ILIKE '%duplicate key value violates unique constraint "unique_id_sinp_unique"%' ;
+    ) ILIKE '%duplicate key value violates unique constraint "unique_id_sinp_unique"%'
+   AND NOT EXISTS (
+        SELECT 'TRUE'
+        FROM gn_synthese.synthese AS s
+        WHERE s.unique_id_sinp = (el.item ->> 'id_perm_sinp')::uuid
+            AND s.id_source <> gn2pg_lpo.fct_c_get_or_insert_source(el."source")
+   ) ;
 
 
 \echo '-------------------------------------------------------------------------------'
 \echo 'Clean error_log from data reinserted into data_json'
 DELETE FROM gn2pg_lpo.error_log AS el
 USING gn2pg_lpo.data_json AS dj
-WHERE dj.source = el.source
-    AND dj.id_data = el.id_data
+WHERE el."source" = dj."source"
+    AND el.id_data = dj.id_data
     AND el.last_ts < dj.update_ts
     AND split_part(
         split_part(el.error::text, ') '::text, 2),
@@ -90,3 +108,23 @@ COMMIT;
 -- 		ON elu."uuid" = s.unique_id_sinp
 -- WHERE dj."uuid" IS NOT NULL
 -- ORDER BY elu.last_ts ;
+
+
+-- Query to find existing UUID in other source :
+-- SELECT
+--     "source",
+--     id_data,
+--     (item ->> 'id_perm_sinp')::uuid,
+--     item
+-- FROM gn2pg_lpo.error_log AS el
+-- WHERE last_ts > '2024-11-01'
+--     AND split_part(
+--         split_part(error::text, ') '::text, 2),
+--         'DETAIL'::text, 1
+--     ) ILIKE '%duplicate key value violates unique constraint "unique_id_sinp_unique"%'
+--    AND EXISTS (
+--         SELECT 'TRUE'
+--         FROM gn_synthese.synthese AS s
+--         WHERE s.unique_id_sinp = (el.item ->> 'id_perm_sinp')::uuid
+--             AND s.id_source <> gn2pg_lpo.fct_c_get_or_insert_source(el."source")
+--    ) ;
