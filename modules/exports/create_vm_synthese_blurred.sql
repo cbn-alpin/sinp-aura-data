cc434670b1f0acf24e8773eb85caedd2b64ce196
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2025-01-22 17:12:24 +0100

    feat : add exports script

---
+++ modules/exports/create_vm_synthese_blurred.sql
@@ -0,0 +1,83 @@
+-- Create fonction to define if geometry has to be blurred regarding to the data's sensitivity
+
+CREATE OR REPLACE FUNCTION gn_exports.is_blurred_area_type_by_sensitivity(nomenclaturecode character varying, areatypecode character varying)
+RETURNS boolean
+LANGUAGE plpgsql
+IMMUTABLE
+AS $function$
+	DECLARE isBlurred boolean;
+
+	BEGIN
+		SELECT INTO isBlurred
+			CASE
+				WHEN ( nomenclatureCode = '1' AND areaTypeCode = 'M5' ) THEN true
+				WHEN ( nomenclatureCode = '2' AND areaTypeCode = 'M5' ) THEN true
+				WHEN ( nomenclatureCode = '3' AND areaTypeCode = 'M5' ) THEN true
+				WHEN ( nomenclatureCode = '2.1' AND areaTypeCode = 'M1' ) THEN true
+				WHEN ( nomenclatureCode = '2.2' AND areaTypeCode = 'M2' ) THEN true
+				WHEN ( nomenclatureCode = '2.3' AND areaTypeCode = 'M5' ) THEN true
+				WHEN ( nomenclatureCode = '2.4' AND areaTypeCode = 'M10' ) THEN true
+				WHEN ( nomenclatureCode = '2.5' AND areaTypeCode = 'M20' ) THEN true
+				WHEN ( nomenclatureCode = '2.6' AND areaTypeCode = 'M50' ) THEN true
+				WHEN ( nomenclatureCode = '2.7' AND areaTypeCode = 'M50' ) THEN true
+				ELSE false
+			END ;
+
+		RETURN isBlurred ;
+	END;
+$function$
+;
+
+
+-- Create fonction to define if geometry has to be blurred
+
+CREATE OR REPLACE FUNCTION gn_exports.is_blurred_area_type(sensicode character varying, areatypecode character varying)
+RETURNS boolean
+LANGUAGE plpgsql
+IMMUTABLE
+AS $function$
+	DECLARE isBlurred boolean;
+
+	BEGIN
+		SELECT INTO isBlurred
+			CASE
+				WHEN sensiCode::NUMERIC >= 1 AND sensiCode::NUMERIC <= 3 THEN
+					gn_exports.is_blurred_area_type_by_sensitivity(sensiCode, areaTypeCode)
+				ELSE false
+			END;
+
+		RETURN isBlurred ;
+	END;
+$function$
+;
+
+
+-- Create materialized view with all synthese geometry blurred depending on sensitivity level
+
+CREATE MATERIALIZED VIEW gn_exports.synthese_blurred
+TABLESPACE pg_default
+AS SELECT
+	sa.id_synthese,
+	sa.id_area,
+	st_transform(a.geom, 4326) AS geom_4326,
+	t.type_code
+FROM gn_synthese.synthese s
+	JOIN gn_synthese.cor_area_synthese sa
+		ON s.id_synthese = sa.id_synthese
+	JOIN ref_nomenclatures.t_nomenclatures sens
+		ON s.id_nomenclature_sensitivity = sens.id_nomenclature
+	LEFT JOIN ref_nomenclatures.t_nomenclatures st
+		ON s.id_nomenclature_observation_status = st.id_nomenclature
+	JOIN ref_geo.l_areas a
+		ON sa.id_area = a.id_area
+	JOIN ref_geo.bib_areas_types t
+		ON a.id_type = t.id_type
+WHERE s.the_geom_point IS NOT NULL
+	AND st.cd_nomenclature = 'Pr'
+	AND sens.cd_nomenclature NOT IN ('4', '2.8')
+	AND t.type_code IN ('M1', 'M2', 'M5', 'M10', 'M20', 'M50')
+	AND atlas.is_blurred_area_type(sens.cd_nomenclature, t.type_code) = TRUE
+WITH DATA;
+
+CREATE UNIQUE INDEX i_synthese_blurred ON gn_exports.synthese_blurred USING btree (id_synthese, id_area);
+CREATE INDEX synthese_blurred_type_code_idx ON gn_exports.synthese_blurred USING btree (type_code);
