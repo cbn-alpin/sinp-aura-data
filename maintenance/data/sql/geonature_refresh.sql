f5367107ac768445eb2a17dad44053f2456ae385
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2025-01-29 17:37:00 +0100

    feat(maintenance): add refresh PNR exports VM

--- maintenance/data/sql/geonature_refresh.sql
+++ maintenance/data/sql/geonature_refresh.sql
@@ -5,11 +5,50 @@
 
 BEGIN ;
 
--- TAXO
-
 \echo '----------------------------------------------------------------'
 \echo 'Refreshing gn_exports.vm_catalog_taxa_region:'
 REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.vm_catalog_taxa_region ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.synthese_blurred:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.synthese_blurred ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_haut_jura_blurred:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_haut_jura_blurred ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_livradois_forez_blurred:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_livradois_forez_blurred ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_massif_bauges_blurred:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_massif_bauges_blurred ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_pilat_blurred:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_pilat_blurred ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_vercors_blurred_1:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_vercors_blurred_1 ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_vercors_blurred_2:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_vercors_blurred_2 ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_volcans_auvergne_blurred_1:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_blurred_1 ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_volcans_auvergne_blurred_2:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_blurred_2 ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_volcans_auvergne_blurred_3:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_blurred_3 ;
+
 
 \echo '----------------------------------------------------------------'
 \echo 'COMMIT if all is ok:'
