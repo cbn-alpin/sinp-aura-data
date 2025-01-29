63171848905ca67bd377dd28328126db3fdfd707
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2025-01-29 17:36:09 +0100

    feat(module export): split export PNR Vercors & volcans Auvergne

---
+++ modules/exports/export_pnr_vercors_1.sql
@@ -0,0 +1,155 @@
+-- PNR Vercors synthese export with geom blurred depending on sensitivity level < 2017 (1)
+
+-- Enable timing
+\timing
+
+BEGIN;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Create materialized view gn_exports.pnr_vercors_blurred_1'
+
+CREATE MATERIALIZED VIEW gn_exports.pnr_vercors_blurred_1 AS
+WITH
+blurred_centroid AS (
+	SELECT
+		sb.id_synthese,
+		st_centroid(st_union(sb.geom_4326)) AS geom_point
+	FROM gn_exports.synthese_blurred AS sb
+	GROUP BY sb.id_synthese
+),
+
+pnr_vercors AS(
+	SELECT
+		UNNEST(
+			ARRAY['26081','26382','26232','26100','26023','38409','38322','38356','38092','38108','38018','26149','26381','26273','26039','38453','38036','38333','38195','26034','26224','38319','38443','26032','26024','26212','38436','38524','38235','38111','38187','38338','38390','38117','38216','38345','38248','38450','38474','38281','38169','38485','38540','38486','26086','26142','26354','26168','26055','26098','26365','26299','26291','26327','26364','26371','26059','26066','26163','26069','26035','26240','26246','26128','26141','26175','26346','26159','26290','26001','26195','26282','26308','26221','26316','26113','38186','38103','38429','26359','26270','26309','26074','26320','26117','26307','26217','26315','26223','26311','26331','26302','38355','38129','38205','38548','38225','38090','38391','38243','38419','38113','38204','38424','38301','38438','38153','38433']
+			) AS code
+),
+
+synthese_export AS (
+	SELECT
+		DISTINCT s.id_synthese,
+		s.the_geom_4326
+	FROM gn_synthese.synthese s
+		LEFT JOIN gn_synthese.cor_area_synthese cas
+			ON s.id_synthese = cas.id_synthese
+		LEFT JOIN ref_geo.l_areas a
+			ON cas.id_area = a.id_area
+		JOIN pnr_vercors p
+			ON a.area_code = p.code
+	WHERE s.date_min < '2017-01-01'
+)
+SELECT
+    s.id_synthese,
+    s.entity_source_pk_value                         AS id_source,
+    s.unique_id_sinp                                 AS id_perm_sinp,
+    s.unique_id_sinp_grp                             AS id_perm_grp_sinp,
+    s.date_min::date                                 AS date_debut,
+    s.date_max::date                                 AS date_fin,
+    s.cd_nom,
+    s.meta_v_taxref                                  AS version_taxref,
+    s.nom_cite,
+    s.count_min                                      AS nombre_min,
+    s.count_max                                      AS nombre_max,
+    s.altitude_min,
+    s.altitude_max,
+    s.depth_min                                      AS profondeur_min,
+    s.depth_max                                      AS profondeur_max,
+    s.observers                                      AS observateurs,
+    s.determiner                                     AS determinateur,
+    s.validator                                      AS validateur,
+    s.comment_context                                AS comment_releve,
+    s.comment_description                            AS comment_occurrence,
+    td.dataset_shortname                             AS jdd_nom_court,
+    td.unique_dataset_id                             AS jdd_uuid,
+    s.reference_biblio,
+    s.cd_hab                                         AS code_habitat,
+    h.lb_hab_fr                                      AS habitat,
+    s.place_name                                     AS nom_lieu,
+    s.precision,
+    s.additional_data                                AS donnees_additionnelles,
+    CASE
+        WHEN bc.geom_point IS NOT NULL
+            THEN st_astext(bc.geom_point)
+        ELSE st_astext(s.the_geom_point)
+    END                                              AS the_geom_point,
+    n1.cd_nomenclature                               AS nature_objet_geo,
+    n2.cd_nomenclature                               AS type_regroupement,
+    s.grp_method                                     AS methode_regroupement,
+    n3.cd_nomenclature                               AS comportement,
+    n4.cd_nomenclature                               AS technique_obs,
+    n5.cd_nomenclature                               AS statut_biologique,
+    n6.cd_nomenclature                               AS etat_biologique,
+    n7.cd_nomenclature                               AS naturalite,
+    n8.cd_nomenclature                               AS preuve_existante,
+    n9.cd_nomenclature                               AS precision_diffusion,
+    n10.cd_nomenclature                              AS stade_vie,
+    n11.cd_nomenclature                              AS sexe,
+    n12.cd_nomenclature                              AS objet_denombrement,
+    n13.cd_nomenclature                              AS type_denombrement,
+    n14.cd_nomenclature                              AS niveau_sensibilite,
+    n15.cd_nomenclature                              AS statut_observation,
+    n16.cd_nomenclature                              AS floutage_dee,
+    n17.cd_nomenclature                              AS statut_source,
+    n18.cd_nomenclature                              AS type_info_geo,
+    n19.cd_nomenclature                              AS methode_determination,
+    n20.cd_nomenclature                              AS statut_validation,
+    coalesce(s.meta_update_date, s.meta_create_date) AS derniere_action
+FROM gn_synthese.synthese AS s
+    JOIN synthese_export se
+        ON se.id_synthese = s.id_synthese
+    JOIN gn_meta.t_datasets td
+        ON td.id_dataset = s.id_dataset
+    LEFT JOIN blurred_centroid AS bc
+        ON bc.id_synthese = s.id_synthese
+    LEFT JOIN ref_habitats.habref h
+        ON h.cd_hab = s.cd_hab
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n1
+        ON s.id_nomenclature_geo_object_nature = n1.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n2
+        ON s.id_nomenclature_grp_typ = n2.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n3
+        ON s.id_nomenclature_behaviour = n3.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n4
+        ON s.id_nomenclature_obs_technique = n4.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n5
+        ON s.id_nomenclature_bio_status = n5.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n6
+        ON s.id_nomenclature_bio_condition = n6.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n7
+        ON s.id_nomenclature_naturalness = n7.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n8
+        ON s.id_nomenclature_exist_proof = n8.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n9
+        ON s.id_nomenclature_diffusion_level = n9.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n10
+        ON s.id_nomenclature_life_stage = n10.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n11
+        ON s.id_nomenclature_sex = n11.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n12
+        ON s.id_nomenclature_obj_count = n12.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n13
+        ON s.id_nomenclature_type_count = n13.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n14
+        ON s.id_nomenclature_sensitivity = n14.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n15
+        ON s.id_nomenclature_observation_status = n15.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n16
+        ON s.id_nomenclature_blurring = n16.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n17
+        ON s.id_nomenclature_source_status = n17.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n18
+        ON s.id_nomenclature_info_geo_type = n18.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n19
+        ON s.id_nomenclature_determination_method = n19.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n20
+        ON s.id_nomenclature_valid_status = n20.id_nomenclature
+WHERE
+    s.the_geom_point IS NOT NULL
+    AND n15.cd_nomenclature = 'Pr'
+    AND n9.cd_nomenclature != '4'
+    AND n14.cd_nomenclature NOT IN ('4', '2.8')
+;
+
+\echo '----------------------------------------------------------------'
+\echo 'COMMIT if all is ok:'
+COMMIT;
---
+++ modules/exports/export_pnr_volcans_auvergne_1.sql
@@ -0,0 +1,156 @@
+-- PNR des volcans d'Auvergne synthese export with geom blurred depending on sensitivity level < 2015 (1)
+
+-- Enable timing
+\timing
+
+BEGIN;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Create materialized view gn_exports.pnr_volcans_auvergne_blurred_1'
+
+
+CREATE MATERIALIZED VIEW gn_exports.pnr_volcans_auvergne_blurred_1 AS
+WITH
+blurred_centroid AS (
+	SELECT
+		sb.id_synthese,
+		st_centroid(st_union(sb.geom_4326)) AS geom_point
+	FROM gn_exports.synthese_blurred AS sb
+	GROUP BY sb.id_synthese
+),
+
+pnr_volcans_auvergne AS(
+	SELECT
+		UNNEST(
+			ARRAY['63470','63397','63236','63305','63129','63440','63401','63198','63026','63070','63092','63381','63097','63084','63449','63396','63103','63417','63254','63290','63285','63219','63385','63071','63386','63257','63020','63326','63083','63451','63077','63264','63407','63123','63380','63383','63345','63093','63274','63189','63246','63153','63247','63169','63144','63117','63028','63098','63038','63192','63047','63263','63346','63336','63279','63259','63087','63299','63303','63134','15219','15146','63122','63006','63313','63220','63007','63009','15141','15086','15112','15026','15098','15238','15067','15202','15176','15174','15208','15020','15178','15113','15101','15038','15049','15205','15006','15075','15116','15162','15081','15139','15155','15258','15180','15201','15192','15236','15066','15050','15061','15100','15138','15248','15148','15095','15096','15070','15052','15190','15091','15173','15263','15041','15025','15132','15059','15077','15033','15152','15218','15243','15008','15185','15124','15092','15170','15129','15240','15009','15110','15040','15114','15054','15225','15253','15213','15102','15080','15137','15249','15105','15256','15126','15043','15151','15001','63335','63017','63466','15069','63126','63302','15244','63308','63075','15053','15127','63141','15141','63183','63042','63127','63426','15015','15246','63248']
+			) AS code
+),
+
+synthese_export AS (
+	SELECT
+		DISTINCT s.id_synthese,
+		s.the_geom_4326
+	FROM gn_synthese.synthese s
+		LEFT JOIN gn_synthese.cor_area_synthese cas
+			ON s.id_synthese = cas.id_synthese
+		LEFT JOIN ref_geo.l_areas a
+			ON cas.id_area = a.id_area
+		JOIN pnr_volcans_auvergne p
+			ON a.area_code = p.code
+	WHERE s.date_min < '2015-01-01'
+)
+SELECT
+    s.id_synthese,
+    s.entity_source_pk_value                         AS id_source,
+    s.unique_id_sinp                                 AS id_perm_sinp,
+    s.unique_id_sinp_grp                             AS id_perm_grp_sinp,
+    s.date_min::date                                 AS date_debut,
+    s.date_max::date                                 AS date_fin,
+    s.cd_nom,
+    s.meta_v_taxref                                  AS version_taxref,
+    s.nom_cite,
+    s.count_min                                      AS nombre_min,
+    s.count_max                                      AS nombre_max,
+    s.altitude_min,
+    s.altitude_max,
+    s.depth_min                                      AS profondeur_min,
+    s.depth_max                                      AS profondeur_max,
+    s.observers                                      AS observateurs,
+    s.determiner                                     AS determinateur,
+    s.validator                                      AS validateur,
+    s.comment_context                                AS comment_releve,
+    s.comment_description                            AS comment_occurrence,
+    td.dataset_shortname                             AS jdd_nom_court,
+    td.unique_dataset_id                             AS jdd_uuid,
+    s.reference_biblio,
+    s.cd_hab                                         AS code_habitat,
+    h.lb_hab_fr                                      AS habitat,
+    s.place_name                                     AS nom_lieu,
+    s.precision,
+    s.additional_data                                AS donnees_additionnelles,
+    CASE
+        WHEN bc.geom_point IS NOT NULL
+            THEN st_astext(bc.geom_point)
+        ELSE st_astext(s.the_geom_point)
+    END                                              AS the_geom_point,
+    n1.cd_nomenclature                               AS nature_objet_geo,
+    n2.cd_nomenclature                               AS type_regroupement,
+    s.grp_method                                     AS methode_regroupement,
+    n3.cd_nomenclature                               AS comportement,
+    n4.cd_nomenclature                               AS technique_obs,
+    n5.cd_nomenclature                               AS statut_biologique,
+    n6.cd_nomenclature                               AS etat_biologique,
+    n7.cd_nomenclature                               AS naturalite,
+    n8.cd_nomenclature                               AS preuve_existante,
+    n9.cd_nomenclature                               AS precision_diffusion,
+    n10.cd_nomenclature                              AS stade_vie,
+    n11.cd_nomenclature                              AS sexe,
+    n12.cd_nomenclature                              AS objet_denombrement,
+    n13.cd_nomenclature                              AS type_denombrement,
+    n14.cd_nomenclature                              AS niveau_sensibilite,
+    n15.cd_nomenclature                              AS statut_observation,
+    n16.cd_nomenclature                              AS floutage_dee,
+    n17.cd_nomenclature                              AS statut_source,
+    n18.cd_nomenclature                              AS type_info_geo,
+    n19.cd_nomenclature                              AS methode_determination,
+    n20.cd_nomenclature                              AS statut_validation,
+    coalesce(s.meta_update_date, s.meta_create_date) AS derniere_action
+FROM gn_synthese.synthese AS s
+    JOIN synthese_export se
+        ON se.id_synthese = s.id_synthese
+    JOIN gn_meta.t_datasets td
+        ON td.id_dataset = s.id_dataset
+    LEFT JOIN blurred_centroid AS bc
+        ON bc.id_synthese = s.id_synthese
+    LEFT JOIN ref_habitats.habref h
+        ON h.cd_hab = s.cd_hab
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n1
+        ON s.id_nomenclature_geo_object_nature = n1.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n2
+        ON s.id_nomenclature_grp_typ = n2.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n3
+        ON s.id_nomenclature_behaviour = n3.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n4
+        ON s.id_nomenclature_obs_technique = n4.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n5
+        ON s.id_nomenclature_bio_status = n5.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n6
+        ON s.id_nomenclature_bio_condition = n6.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n7
+        ON s.id_nomenclature_naturalness = n7.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n8
+        ON s.id_nomenclature_exist_proof = n8.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n9
+        ON s.id_nomenclature_diffusion_level = n9.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n10
+        ON s.id_nomenclature_life_stage = n10.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n11
+        ON s.id_nomenclature_sex = n11.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n12
+        ON s.id_nomenclature_obj_count = n12.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n13
+        ON s.id_nomenclature_type_count = n13.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n14
+        ON s.id_nomenclature_sensitivity = n14.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n15
+        ON s.id_nomenclature_observation_status = n15.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n16
+        ON s.id_nomenclature_blurring = n16.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n17
+        ON s.id_nomenclature_source_status = n17.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n18
+        ON s.id_nomenclature_info_geo_type = n18.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n19
+        ON s.id_nomenclature_determination_method = n19.id_nomenclature
+    LEFT JOIN ref_nomenclatures.t_nomenclatures n20
+        ON s.id_nomenclature_valid_status = n20.id_nomenclature
+WHERE
+    s.the_geom_point IS NOT NULL
+    AND n15.cd_nomenclature = 'Pr'
+    AND n9.cd_nomenclature != '4'
+    AND n14.cd_nomenclature NOT IN ('4', '2.8')
+;
+
+\echo '----------------------------------------------------------------'
+\echo 'COMMIT if all is ok:'
+COMMIT;
