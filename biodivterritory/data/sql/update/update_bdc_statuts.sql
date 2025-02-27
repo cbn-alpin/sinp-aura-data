8017a2b8583983c6801f2337160cdb3374a192ab
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-02-27 18:44:33 +0100

    fix(biodivterritory): avoid to duplicate rows in taxref_protection_especes

--- biodivterritory/data/sql/update/update_bdc_statuts.sql
+++ biodivterritory/data/sql/update/update_bdc_statuts.sql
@@ -4,6 +4,13 @@
 \echo 'GeoNature database compatibility : v2.4.1'
 BEGIN;
 
+\echo '-------------------------------------------------------------------------------'
+\echo 'Delete constraint on taxref_protection_especes'
+ALTER TABLE taxonomie.taxref_protection_especes DROP CONSTRAINT taxref_protection_especes_pkey ;
+
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Update taxref_protection_especes taxonomy => duplicate rows'
 -- Get valid cd_nom
 WITH linkage AS (
     SELECT
@@ -21,7 +28,7 @@ patchs AS (
         tx.lb_nom AS nom_cite,
         SPLIT_PART(tx.nom_vern, ', ', 1) AS nom_francais_cite
     FROM linkage
-        LEFT JOIN taxonomie.taxref AS tx
+        JOIN taxonomie.taxref AS tx
             ON linkage.cd_ref = tx.cd_nom
 )
 -- Run the update
@@ -31,6 +38,30 @@ UPDATE taxonomie.taxref_protection_especes SET
     nom_francais_cite = patchs.nom_francais_cite
 FROM patchs
 WHERE taxref_protection_especes.cd_nom = patchs.cd_nom_old ;
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Add a temporary unique primarykey column on taxref_protection_especes'
+ALTER TABLE taxonomie.taxref_protection_especes
+ADD COLUMN gid INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY;
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Keep only one row for duplicate rows'
+DELETE FROM taxonomie.taxref_protection_especes AS tpe1
+USING taxonomie.taxref_protection_especes AS tpe2
+WHERE tpe1.gid < tpe2.gid
+	AND tpe1.cd_nom = tpe2.cd_nom
+	AND tpe1.cd_protection = tpe2.cd_protection
+	AND tpe1.cd_nom_cite = tpe2.cd_nom_cite ;
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Delete temporary unique primarykey column from taxref_protection_especes'
+ALTER TABLE taxonomie.taxref_protection_especes
+DROP COLUMN gid ;
+
+\echo '-------------------------------------------------------------------------------'
+\echo 'Restore the original constraint on taxref_protection_especes'
+ALTER TABLE taxonomie.taxref_protection_especes
+ADD CONSTRAINT taxref_protection_especes_pkey PRIMARY KEY (cd_nom, cd_protection, cd_nom_cite) ;
 
 \echo '-------------------------------------------------------------------------------'
 \echo 'COMMIT if all is ok:'
