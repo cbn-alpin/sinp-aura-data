e116561d6a7cdcc940469f820e5fe2462d9a0da8
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2025-04-17 17:27:00 +0200

    feat(export): update geonature refresh VM

--- maintenance/data/sql/geonature_refresh.sql
+++ maintenance/data/sql/geonature_refresh.sql
@@ -10,44 +10,52 @@ BEGIN ;
 REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.catalog_taxa_region ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.synthese_blurred:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.synthese_blurred ;
+\echo 'Refreshing gn_exports.synthese:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.synthese ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_haut_jura_blurred:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_haut_jura_blurred ;
+\echo 'Refreshing gn_exports.pnr_haut_jura:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_haut_jura ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_livradois_forez_blurred:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_livradois_forez_blurred ;
+\echo 'Refreshing gn_exports.pnr_livradois_forez_1:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_livradois_forez_1 ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_massif_bauges_blurred:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_massif_bauges_blurred ;
+\echo 'Refreshing gn_exports.pnr_livradois_forez_2:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_livradois_forez_2 ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_pilat_blurred:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_pilat_blurred ;
+\echo 'Refreshing gn_exports.pnr_massif_bauges:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_massif_bauges ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_vercors_blurred_1:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_vercors_blurred_1 ;
+\echo 'Refreshing gn_exports.pnr_pilat:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_pilat ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_vercors_blurred_2:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_vercors_blurred_2 ;
+\echo 'Refreshing gn_exports.pnr_vercors_1:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_vercors_1 ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_volcans_auvergne_blurred_1:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_blurred_1 ;
+\echo 'Refreshing gn_exports.pnr_vercors_2:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_vercors_2 ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_volcans_auvergne_blurred_2:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_blurred_2 ;
+\echo 'Refreshing gn_exports.pnr_volcans_auvergne_1:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_1 ;
 
 \echo '----------------------------------------------------------------'
-\echo 'Refreshing gn_exports.pnr_volcans_auvergne_blurred_3:'
-REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_blurred_3 ;
+\echo 'Refreshing gn_exports.pnr_volcans_auvergne_2:'
+REFRESH MATERIALIZED VIEW CONCURRENTLY gn_exports.pnr_volcans_auvergne_2 ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_volcans_auvergne_3:'
+REFRESH MATERVIEW CONCIALIZED URRENTLY gn_exports.pnr_volcans_auvergne_3 ;
+
+\echo '----------------------------------------------------------------'
+\echo 'Refreshing gn_exports.pnr_volcans_auvergne_4:'
+REFRESH MATERVIEW CONCIALIZED URRENTLY gn_exports.pnr_volcans_auvergne_4 ;
 
 \echo '----------------------------------------------------------------'
 \echo 'Refreshing gn_exports.lo_parvi_1:'
