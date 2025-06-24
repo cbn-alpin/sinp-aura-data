1b71326ff8784f3188a288f228f558136a18afdb
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-06-24 11:53:20 +0200

    feat(taxref): add initial sql for taxref v18

---
+++ taxref/v18/sql/post_detection.sql
@@ -0,0 +1,16 @@
+-- Mise à jour TaxRef v17 vers v18 pour le SINP AURA
+
+-- Enable trigger "tri_meta_dates_change_synthese"
+ALTER TABLE gn_synthese.synthese ENABLE TRIGGER tri_meta_dates_change_synthese ;
+
+-- Enable trigger "tri_update_calculate_sensitivity"
+ALTER TABLE gn_synthese.synthese ENABLE TRIGGER tri_update_calculate_sensitivity ;
+
+-- Update TaxRef version in t_parameters
+UPDATE gn_commons.t_parameters
+SET parameter_value = 'Taxref V18.0'
+WHERE parameter_name = 'taxref_version' ;
+
+-- Avoid to crash script when we retry applying this script
+DELETE FROM taxonomie.t_meta_taxref
+WHERE referencial_name = 'Taxref' AND version = '18' ;
---
+++ taxref/v18/sql/pre_detection.sql
@@ -0,0 +1,6 @@
+-- Mise à jour TaxRef v17 vers v18 pour le SINP AURA
+BEGIN;
+
+-- -------------------------------------------------------------------------------------------------
+-- Commit if all good
+COMMIT;
