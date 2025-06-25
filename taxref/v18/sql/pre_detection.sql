3b9356f65790f0fc5a3cf4739ee6b46bf2cbff98
Arnaud Ungaro <github-aungaro@arnaud-ungaro.fr>
2025-06-25 10:52:44 +0200

    TaxRefv18

--- taxref/v18/sql/pre_detection.sql
+++ taxref/v18/sql/pre_detection.sql
@@ -75,4 +75,54 @@ UPDATE taxonomie.t_medias SET cd_ref = 128268 WHERE cd_ref IN (128275,142037,142
 DELETE FROM taxonomie.cor_nom_liste WHERE id_nom IN (SELECT id_nom FROM taxonomie.bib_noms WHERE cd_nom IN (128275,142037,142038));
 DELETE FROM taxonomie.bib_noms WHERE cd_nom IN (128275,142037,142038);
 
+
+-- TABLE : gn_sensitivity.t_sensitivity_rules
+DELETE FROM gn_sensitivity.t_sensitivity_rules WHERE cd_nom = 124413;
+UPDATE gn_sensitivity.t_sensitivity_rules SET cd_nom = 138121 WHERE cd_nom = 718726;
+
+-- TABLE : gn_synthese.synthese
+UPDATE gn_synthese.synthese SET cd_nom = 159607 WHERE cd_nom = 92267;
+UPDATE gn_synthese.synthese SET cd_nom = 110473 WHERE cd_nom = 110474;
+UPDATE gn_synthese.synthese SET cd_nom = 1056537 WHERE cd_nom = 117281;
+UPDATE gn_synthese.synthese SET cd_nom = 621429 WHERE cd_nom = 129770;
+UPDATE gn_synthese.synthese SET cd_nom = 773729 WHERE cd_nom = 136960;
+UPDATE gn_synthese.synthese SET cd_nom = 457300 WHERE cd_nom = 233651;
+UPDATE gn_synthese.synthese SET cd_nom = 233652 WHERE cd_nom = 457301;
+UPDATE gn_synthese.synthese SET cd_nom = 233656 WHERE cd_nom = 457302;
+UPDATE gn_synthese.synthese SET cd_nom = 57077 WHERE cd_nom = 658461;
+UPDATE gn_synthese.synthese SET cd_nom = 59428 WHERE cd_nom = 660113;
+UPDATE gn_synthese.synthese SET cd_nom = NULL WHERE cd_nom IN (
+  41508,46412,46608,59404,96518,98692,99589,104154,110344,110424,110991,114417,117011,119429,
+  122827,124262,124413,126163,126212,129108,129226,129579,131837,138395,660054,660095,873328,945104,233536,234037,147083,162283
+);
+
+-- TABLE : taxonomie.bib_noms et cor_nom_liste
+UPDATE taxonomie.bib_noms SET cd_nom = 159607 WHERE cd_nom = 92267;
+UPDATE taxonomie.bib_noms SET cd_nom = 110473 WHERE cd_nom = 110474;
+UPDATE taxonomie.bib_noms SET cd_nom = 1056537 WHERE cd_nom = 117281;
+UPDATE taxonomie.bib_noms SET cd_nom = 614188 WHERE cd_nom = 125814;
+UPDATE taxonomie.bib_noms SET cd_nom = 457300 WHERE cd_nom = 233651;
+UPDATE taxonomie.bib_noms SET cd_nom = 233652 WHERE cd_nom = 457301;
+UPDATE taxonomie.bib_noms SET cd_nom = 233656 WHERE cd_nom = 457302;
+UPDATE taxonomie.bib_noms SET cd_nom = 57077 WHERE cd_nom = 658461;
+UPDATE taxonomie.bib_noms SET cd_nom = 59428 WHERE cd_nom = 660113;
+UPDATE taxonomie.bib_noms SET cd_nom = 773729 WHERE cd_nom = 136960;
+UPDATE taxonomie.bib_noms SET cd_nom = 621429 WHERE cd_nom = 129770;
+
+DELETE FROM taxonomie.cor_nom_liste WHERE id_nom IN (SELECT id_nom FROM taxonomie.bib_noms WHERE cd_nom IN (
+  41508,46412,46608,59404,83018,87931,88315,96518,98692,99589,103706,104146,104154,104286,
+  110344,110424,110991,114417,117011,119398,119429,121607,122827,124262,124413,126163,
+  126212,129108,129226,129530,129579,130237,130415,131837,147083,162283,233536,234037,461959,620446,
+  660054,660095,719293,873328,945104,138395
+));
+DELETE FROM taxonomie.bib_noms WHERE cd_nom IN (
+  41508,46412,46608,59404,83018,87931,88315,96518,98692,99589,103706,104146,104154,104286,
+  110344,110424,110991,114417,117011,119398,119429,121607,122827,124262,124413,126163,
+  126212,129108,129226,129530,129579,130237,130415,131837,147083,162283,233536,234037,461959,620446,
+  660054,660095,719293,873328,945104,138395
+);
+
+-- =====================================================================
+
+
 COMMIT;
