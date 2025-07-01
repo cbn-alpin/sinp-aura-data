f84460608a48c7091cde554bcff04822ce35a025
Arnaud Ungaro <github-aungaro@arnaud-ungaro.fr>
2025-07-01 10:56:13 +0200

    taxrefv18

--- taxref/v18/sql/reactivation_contraintes_apres_taxref.sql
+++ taxref/v18/sql/reactivation_contraintes_apres_taxref.sql
@@ -4,22 +4,11 @@ BEGIN;
 ALTER TABLE gn_synthese.synthese ENABLE TRIGGER tri_meta_dates_change_synthese;
 ALTER TABLE gn_synthese.synthese ENABLE TRIGGER tri_update_calculate_sensitivity;
 
--- Rétablissement de la contrainte FK sur cd_nom de gn_synthese.synthese
-ALTER TABLE gn_synthese.synthese
-    ADD CONSTRAINT fk_synthese_cd_nom
-    FOREIGN KEY (cd_nom) REFERENCES taxonomie.taxref(cd_nom);
-
--- Rétablissement de la contrainte FK sur cd_nom de taxonomie.bib_noms
-ALTER TABLE taxonomie.bib_noms
-    ADD CONSTRAINT fk_bib_nom_taxref
-    FOREIGN KEY (cd_nom) REFERENCES taxonomie.taxref(cd_nom);
-
--- Rétablissement de la contrainte FK sur cd_ref de taxonomie.t_medias
-ALTER TABLE taxonomie.t_medias
-    ADD CONSTRAINT fk_t_media_bib_noms
-    FOREIGN KEY (cd_ref) REFERENCES taxonomie.bib_noms(cd_nom);
-
--- (Optionnel) Rétablissement de la contrainte CHECK si elle existait avant
--- ALTER TABLE taxonomie.t_medias ADD CONSTRAINT check_is_cd_ref CHECK (cd_ref > 0);
+-- Ajout des contraintes (remplacement)
+ALTER TABLE gn_synthese.synthese ADD CONSTRAINT fk_synthese_cd_nom FOREIGN KEY (cd_nom) REFERENCES taxonomie.taxref(cd_nom) ON UPDATE CASCADE;
+ALTER TABLE taxonomie.t_medias ADD CONSTRAINT fk_t_media_bib_noms FOREIGN KEY (cd_ref) REFERENCES taxonomie.bib_noms(cd_nom) MATCH FULL ON UPDATE CASCADE;
+ALTER TABLE taxonomie.t_medias ADD CONSTRAINT check_is_cd_ref CHECK ((cd_ref = taxonomie.find_cdref(cd_ref)));
+ALTER TABLE taxonomie.bib_noms ADD CONSTRAINT fk_bib_nom_taxref FOREIGN KEY (cd_nom) REFERENCES taxonomie.taxref(cd_nom);
+ALTER TABLE taxonomie.bib_noms ADD CONSTRAINT bib_noms_cd_nom_key UNIQUE (cd_nom);
 
 COMMIT;
