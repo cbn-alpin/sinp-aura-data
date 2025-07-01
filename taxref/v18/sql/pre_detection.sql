e26d83b39119271180cf58405ed8345eaf079790
Arnaud Ungaro <github-aungaro@arnaud-ungaro.fr>
2025-07-01 11:04:31 +0200

    taxrefv18

--- taxref/v18/sql/pre_detection.sql
+++ taxref/v18/sql/pre_detection.sql
@@ -17,7 +17,7 @@ ALTER TABLE taxonomie.bib_noms DROP CONSTRAINT IF EXISTS fk_bib_nom_taxref;
 -- ----------------------------------------------------------------
 -- 1. Asparagus officinalis (cible : 84279)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (131756, 84279)
+WHERE cd_ref IN (131756)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -29,7 +29,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 84279 WHERE cd_ref IN (131756);
 -- ----------------------------------------------------------------
 -- 2. Juncus bulbosus (cible : 104145)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (104156,104225,104345,104367,113317,136927,136928,136955,136957,147921,147923,104145)
+WHERE cd_ref IN (104156,104225,104345,104367,113317,136927,136928,136955,136957,147921,147923)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -41,7 +41,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 104145 WHERE cd_ref IN (104156,104225,104
 -- ----------------------------------------------------------------
 -- 3. Neotinea ustulata (cible : 109501)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (111012,138429,162137,718722,109501)
+WHERE cd_ref IN (111012,138429,162137,718722)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -53,7 +53,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 109501 WHERE cd_ref IN (111012,138429,162
 -- ----------------------------------------------------------------
 -- 4. Pulsatilla rubra (cible : 116456)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (82660,131408,139522,150342,150343,150351,155095,161705,718641,718642,116456)
+WHERE cd_ref IN (82660,131408,139522,150342,150343,150351,155095,161705,718641,718642)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -65,7 +65,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 116456 WHERE cd_ref IN (82660,131408,1395
 -- ----------------------------------------------------------------
 -- 5. Reseda lutea (cible : 117458)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (117447,139857,150557,117458)
+WHERE cd_ref IN (117447,139857,150557)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -77,7 +77,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 117458 WHERE cd_ref IN (117447,139857,150
 -- ----------------------------------------------------------------
 -- 6. Rubia peregrina (cible : 118916)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (118909,140175,118916)
+WHERE cd_ref IN (118909,140175)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -89,7 +89,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 118916 WHERE cd_ref IN (118909,140175);
 -- ----------------------------------------------------------------
 -- 7. Salix fragilis (cible : 120040)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (119946,119950,120263,120512,140439,151070,151071,120040)
+WHERE cd_ref IN (119946,119950,120263,120512,140439,151070,151071)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -101,7 +101,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 120040 WHERE cd_ref IN (119946,119950,120
 -- ----------------------------------------------------------------
 -- 8. Solanum nigrum (cible : 124080)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (124017,124083,141265,141273,141275,151896,124080)
+WHERE cd_ref IN (124017,124083,141265,141273,141275,151896)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -113,7 +113,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 124080 WHERE cd_ref IN (124017,124083,141
 -- ----------------------------------------------------------------
 -- 9. Thalictrum simplex (cible : 126213)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (126089,141635,141636,141638,718422,126213)
+WHERE cd_ref IN (126089,141635,141636,141638,718422)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
@@ -125,7 +125,7 @@ UPDATE taxonomie.t_medias SET cd_ref = 126213 WHERE cd_ref IN (126089,141635,141
 -- ----------------------------------------------------------------
 -- 10. Urtica dioica (cible : 128268)
 DELETE FROM taxonomie.cor_taxon_attribut 
-WHERE cd_ref IN (128275,142037,142038,128268)
+WHERE cd_ref IN (128275,142037,142038)
   AND id_attribut IN (
         taxonomie.get_id_attribut_by_name('sinp_description'),
         taxonomie.get_id_attribut_by_name('sinp_ecology'),
