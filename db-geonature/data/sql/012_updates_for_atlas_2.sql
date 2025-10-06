ded42022b5775cf3dc17824a21ffe0a908c218b4
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-10-06 16:39:05 +0200

    fix(db-geonature): add ref_geo table for Atlas v2.0

--- db-geonature/data/sql/012_updates_for_atlas_2.sql
+++ db-geonature/data/sql/012_updates_for_atlas_2.sql
@@ -9,9 +9,24 @@
 
 BEGIN ;
 
-ALTER TABLE ref_geo.l_areas
-    ADD COLUMN IF NOT EXISTS geom_4326 public.geometry(multipolygon, 4326) OPTIONS(column_name 'geom_4326') NULL,
-	ADD COLUMN IF NOT EXISTS description text OPTIONS(column_name 'description') NULL ;
+CREATE TABLE IF NOT EXISTS ref_geo.cor_areas (
+    id_area_group int4 NULL,
+    id_area int4 NULL,
+    CONSTRAINT fk_ref_geo_cor_areas_id_area
+        FOREIGN KEY (id_area) REFERENCES ref_geo.l_areas(id_area)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_ref_geo_cor_areas_id_area_group
+        FOREIGN KEY (id_area_group) REFERENCES ref_geo.l_areas(id_area)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS ref_geo_cor_areas_id_area ON ref_geo.cor_areas USING btree (id_area);
+
+CREATE INDEX IF NOT EXISTS ref_geo_cor_areas_id_area_group ON ref_geo.cor_areas USING btree (id_area_group);
+
+ALTER TABLE ref_geo.l_areas ADD COLUMN IF NOT EXISTS geom_4326 public.geometry(multipolygon, 4326) NULL;
+
+ALTER TABLE ref_geo.l_areas ADD COLUMN IF NOT EXISTS "description" text NULL;
 
 UPDATE ref_geo.l_areas
 SET geom_4326 = st_transform(geom, 4326)
