9bbf32bb2163ccd67e25e970bfdff2754c3d29ba
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2026-03-20 20:44:20 +0100

    fix(extracts): improve extract validations from synthese by source

--- extracts/synthese_validation_by_source.sql
+++ extracts/synthese_validation_by_source.sql
@@ -6,19 +6,22 @@
 COPY (
     SELECT
         s.unique_id_sinp,
-        ref_nomenclatures.get_cd_nomenclature(s.id_nomenclature_valid_status) AS code_nomenclature_valid_status,
-        s."validator",
-        s.validation_comment AS comment,
+        ref_nomenclatures.get_cd_nomenclature(
+            s.id_nomenclature_valid_status
+        ) AS code_nomenclature_valid_status,
+        COALESCE(s."validator", '\N') AS "validator",
+        COALESCE(s.validation_comment, '\N') AS comment,
         gn_commons.determine_auto_validation(
             s."validator", s.meta_validation_date, s.id_nomenclature_valid_status
         ) AS "automatic",
-        date_trunc('second', s.meta_validation_date)::text AS creation_date,
-        NULL AS additional_data,
+        COALESCE(date_trunc('second', s.meta_validation_date)::text, '\N') AS creation_date,
+        '\N' AS additional_data,
         date_trunc('second', s.meta_create_date)::text AS meta_create_date,
         date_trunc('second', s.meta_update_date)::text AS meta_update_date,
         'I' AS meta_last_action
     FROM gn_synthese.synthese AS s
-    WHERE s.id_nomenclature_valid_status IS NOT NULL
+    WHERE s.unique_id_sinp IS NOT NULL
+        AND s.id_nomenclature_valid_status IS NOT NULL
         AND s.id_nomenclature_valid_status != ref_nomenclatures.get_id_nomenclature('STATUT_VALID', '0')
         AND s.id_source = gn_synthese.get_id_source_by_name('${source}')
 ) TO stdout
